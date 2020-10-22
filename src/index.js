2387365d6936c935334e82eb6f26fe7ffe8af6db
MassimmoMaratty <avanty_87@bk.ru>
2020-10-22 23:41:27 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let array = bracketsConfig.map(el => el.join(""));
+  for (i = 0; i < array.length; ) {
+    if (str.includes(array[i])) {
+      str = str.replace(array[i], "");
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+  return !str;
+};
